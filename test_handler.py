0deea667b02f580b013cce7734269bf81248bdd1
arkady <arkady>
2021-12-06 18:32:48 +0200

    pytest

--- test_handler.py
+++ test_handler.py
@@ -2,5 +2,20 @@ import pytest
 
 import handler
 
+
 def test_region_to_filename():
     assert handler.region_to_filename("test") == "test.json"
+
+
+validate_region_testdata = [
+    ("eu-west-1", True),
+    ("a", False),
+    ("eu-west-1 ", False),
+    ("eu1-west-1", False),
+    ("e-west-12", False),
+]
+
+
+@pytest.mark.parametrize("s, expected", validate_region_testdata)
+def test_validate_region(s, expected):
+    assert handler.validate_region(s) == expected

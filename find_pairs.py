31c06072db05978ffc3aeb53fc93a6242259d845
Arkady Miasnikov <larytet@gmail.com>
2021-12-14 21:39:33 +0200

    Update find_pairs.py

--- find_pairs.py
+++ find_pairs.py
@@ -16,21 +16,20 @@ def test_main():
     input = [9, -11, 0, -7, 4, -3, 2, 2, -8, -2, 3, 3, 7, -2, 3, -2, -9, 11, 0]
     expected_output = [7, 3, 2, 2, 9, 11]
 
-    negative_numbers, positive_numbers = {}, {}
+    counters = {}
     output: List[int] = []
 
     for val in input:
         if not val:
             continue
-        _dict_sibling, _dict = positive_numbers, negative_numbers
-        if val < 0:
-            _dict_sibling, _dict = _dict, _dict_sibling
-        val = abs(val)
-        if _dict_sibling.get(val, 0):
-            output.append(val)
-            _dict_sibling[val] -= 1
-        else:
-            _dict[val] = _dict.get(val, 0) + 1
+
+        if counters.get(-val, 0):
+            counters[-val] -= 1
+            output.append(abs(val))
+            continue
+
+        counters[val] = counters.get(val, 0) + 1
+
 
     output.sort()
     expected_output.sort()

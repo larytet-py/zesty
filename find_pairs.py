009a0dfdb072028e73139984cd7ff4c37d13c75d
Arkady Miasnikov <larytet@gmail.com>
2021-12-15 07:43:21 +0200

    Update find_pairs.py

--- find_pairs.py
+++ find_pairs.py
@@ -11,9 +11,15 @@ from typing import Dict, List
 #  * Sort the original array
 #  * Move in two directions from left to right and from right to left
 #  * Match pairs, skip numbers without matches
+#  No additional space, O(nlg(n)) complexity
 
 
 def test_main():
+    """
+    Read the list once, collect the numbers appearences in a dictionary, remove matching 
+    numbers
+    O(n) space, O(n) complexity
+    """
     input = [9, -11, 0, -7, 4, -3, 2, 2, -8, -2, 3, 3, 7, -2, 3, -2, -9, 11, 0]
     expected_output = [7, 3, 2, 2, 9, 11]
 
